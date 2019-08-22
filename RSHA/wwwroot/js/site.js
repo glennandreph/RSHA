f72ee78380005b90f904d7114bfa18da740abddc
Glenn Andre <31070231+glennandreph@users.noreply.github.com>
2019-08-22 12:45:40 +0200

    Add files via upload

---
+++ RSHA/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// navigation bar
+
+window.onscroll = function () {
+    stickyNav()
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop + 250;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.add("sticky-gone")
+        navbar.classList.remove("sticky");
+    }
+};
