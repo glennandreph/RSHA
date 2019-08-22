b39ec43af439f0db916362883bc59e6492d8ce46
Glenn Andre <31070231+glennandreph@users.noreply.github.com>
2019-08-22 20:00:16 +0200

    Add files via upload

---
+++ RSHA/Utilities/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSHA.Utilities
+{
+    public class StaticDetails
+    {
+        public const string MechanicEndUser = "Mechanic";
+        public const string AdminEndUser = "Admin";
+    }
+}
