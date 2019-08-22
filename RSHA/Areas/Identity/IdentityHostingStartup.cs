b496d5d72a134b2a8c776247c6d57688cf4004f3
Glenn Andre <31070231+glennandreph@users.noreply.github.com>
2019-08-22 20:03:50 +0200

    Add files via upload

---
+++ RSHA/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RSHA.Data;
+
+[assembly: HostingStartup(typeof(RSHA.Areas.Identity.IdentityHostingStartup))]
+namespace RSHA.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
