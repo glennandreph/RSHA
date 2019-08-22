3cc3d8fc868ed1b212fc423b6336f85000be6081
Glenn Andre <31070231+glennandreph@users.noreply.github.com>
2019-08-22 20:02:05 +0200

    Add files via upload

---
+++ RSHA/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RSHA.Models;
+
+namespace RSHA.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Requests> Requests { get; set; }
+        public DbSet<ProblemTypes> ProblemTypes { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<Mechanics> Mechanics { get; set; }
+        public DbSet<Feedbacks> Feedbacks { get; set; }
+        public DbSet<BugRapports> BugRapports { get; set; }
+    }
+}
