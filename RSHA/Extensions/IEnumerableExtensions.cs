fc9d26858fd6fd7e0c42770332efdf97894d6dbf
Glenn Andre <31070231+glennandreph@users.noreply.github.com>
2019-08-22 20:01:43 +0200

    Add files via upload

---
+++ RSHA/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RSHA.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItems<T>(this IEnumerable<T> items, int selectedValue)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("Name"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
+                   };
+        }
+
+        public static IEnumerable<SelectListItem> ToSelectListItemsString<T>(this IEnumerable<T> items, string selectedValue)
+        {
+            if (selectedValue == null)
+            {
+                selectedValue = "";
+            }
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("Name"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
+                   };
+        }
+
+    }
+}
